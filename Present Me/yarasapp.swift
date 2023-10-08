e4728205083a740c9575783da3f1390559833d43
Yara <s.yara.sultan@gmail.com>
2023-10-08 17:48:03 +0300

    test with yara

--- Present Me/yarasapp.swift
+++ Present Me/yarasapp.swift
@@ -5,4 +5,37 @@
 //  Created by Yara on 08/10/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct yarasapp: View {
+    var body: some View {
+        
+        HStack {
+            Image("Yara")
+                .resizable()
+                .frame(width: 300, height:300)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.red,lineWidth:10))
+                .padding()
+            
+        }
+        
+        VStack() {
+            Text("Yara Altukhaim")
+            .font(.system(size: 36, weight: .black, design:.serif))
+            Text("Hello, I am an IS Student at King Saud University. I like video games and literature. My rule of life is to always Carpe Diem!")
+                .font(.custom("AmericanTypewriter",fixedSize: 20))
+               
+            
+        }
+        
+        
+        .padding()
+        
+    }
+}
+
+    #Preview {
+        yarasapp()
+    }
+
