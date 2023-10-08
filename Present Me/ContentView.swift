55cbacfe40c8a1977d3b2fd15dd0bb48f94da377
Jood Areabi <areabijoodschl@gmail.com>
2023-10-08 17:09:20 +0300

    text

--- Present Me/ContentView.swift
+++ Present Me/ContentView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            HStack {
+                Image("jood")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.green,lineWidth:5
+                                            ))
+            }
+            
+        VStack(alignment: .leading) {
+                Text("JOOD AREABI")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .fontWeight(.bold)
+                Text("hi! my name is jood, i'm a 19 year old student in PNU, i love earth colors and i occasionally make jewerely!")
+                
+            }
+            .padding()
+   
     }
 }
-
 #Preview {
     ContentView()
 }
