3bf28a11609f8db485e0dca61b8b3bf133df69ea
Jood Areabi <areabijoodschl@gmail.com>
2023-10-08 15:30:11 +0300

    Initial Commit

---
+++ Present Me/Present_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Present_MeApp.swift
+//  Present Me
+//
+//  Created by jood on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Present_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
